6df6f1bb8b538f885cf80c89408d316a59ffd387
Konrad <webmastering1997@gmail.com>
2018-05-27 19:11:59 +0200

    Wypozyczalnia

---
+++ public_html/js/slide.js
@@ -0,0 +1,39 @@
+'use strict';
+
+$(function() {
+
+    //settings for slider
+    var width = 720;
+    var animationSpeed = 1000;
+    var pause = 3000;
+    var currentSlide = 1;
+
+    //cache DOM elements
+    var $slider = $('#slideshow');
+    var $slideContainer = $('.slideshow', $slider);
+    var $slides = $('.slideshow', $slider);
+
+    var interval;
+
+    function startSlider() {
+        interval = setInterval(function() {
+            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+                if (++currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            });
+        }, pause);
+    }
+    function pauseSlider() {
+        clearInterval(interval);
+    }
+
+    $slideContainer
+        .on('mouseenter', pauseSlider)
+        .on('mouseleave', startSlider);
+
+    startSlider();
+
+
+});
